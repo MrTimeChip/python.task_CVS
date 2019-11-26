a1c8a6c97ceb47950a720981303d7a54b9355f58
Данил Панков <mistertimechip@mail.ru>
2019-11-27 00:49:32 +0300

    Added head

--- data_objects/repostitory.py
+++ data_objects/repostitory.py
@@ -1,3 +1,4 @@
+from cvs_trees.head import Head
 from data_objects.branch import Branch
 from data_objects.commit import Commit
 
@@ -7,6 +8,7 @@ class Repository:
     def __init__(self):
         self.current_branch = Branch("NONE")
         self.last_commit = Commit("NONE")
+        self.head = Head()
 
     def add_commit(self, commit):
         if self.last_commit is None:
@@ -15,12 +17,15 @@ class Repository:
         commit.set_previous_commit(self.last_commit)
         self.last_commit = commit
 
+    def reset_head(self):
+        self.head.reset()
+
     def point_to_last_commit(self):
         self.current_branch.set_current_commit(self.last_commit)
 
-    def init(self, head):
+    def init(self):
         self.current_branch = Branch("master")
-        head.set_branch(self.current_branch)
+        self.head.set_branch(self.current_branch)
 
     def get_commit_history(self) -> str:
         return self.last_commit.print_info()
