ac7bc8bb3cff82076afa6393d3841afffc8ed244
Данил Панков <mistertimechip@mail.ru>
2019-11-27 00:46:32 +0300

    Finished repository

--- data_objects/repostitory.py
+++ data_objects/repostitory.py
@@ -1,4 +1,26 @@
+from data_objects.branch import Branch
+from data_objects.commit import Commit
+
+
 class Repository:
 
+    def __init__(self):
+        self.current_branch = Branch("NONE")
+        self.last_commit = Commit("NONE")
+
     def add_commit(self, commit):
-        pass
+        if self.last_commit is None:
+            self.last_commit = commit
+            return
+        commit.set_previous_commit(self.last_commit)
+        self.last_commit = commit
+
+    def point_to_last_commit(self):
+        self.current_branch.set_current_commit(self.last_commit)
+
+    def init(self, head):
+        self.current_branch = Branch("master")
+        head.set_branch(self.current_branch)
+
+    def get_commit_history(self) -> str:
+        return self.last_commit.print_info()
