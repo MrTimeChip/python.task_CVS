5a7a114e799a3a2cdcba062e4e17f8d9390ab2a8
Данил Панков <mistertimechip@mail.ru>
2019-11-27 01:42:48 +0300

    Finished index copying files

--- cvs_trees/index.py
+++ cvs_trees/index.py
@@ -1,4 +1,5 @@
 import os
+from shutil import copyfile
 
 from cvs_trees.head import Head
 from data_objects.commit import Commit
@@ -8,16 +9,25 @@ class Index:
 
     def __init__(self):
         self.working_path = ''
+        self.cvs_path = ""
+        self.index_path = ""
         self.indexed_files = set()
         self.last_commit = Commit("NONE")
 
     def set_working_path(self, path):
         self.working_path = path
 
+    def set_cvs_path(self, path):
+        self.cvs_path = path
+        self.index_path = os.path.join(self.cvs_path, '/INDEX')
+
     def add_new_file(self, filename):
         if not self.is_file_in_working_directory(filename):
             raise FileNotFoundError("No such file in file directory!")
         self.indexed_files.add(filename)
+        source_file = os.path.join(self.working_path, filename)
+        file_copy = os.path.join(self.index_path, filename)
+        copyfile(source_file, file_copy)
 
     def is_file_in_working_directory(self, filename) -> bool:
         return os.path.exists(os.path.join(self.working_path, filename))
