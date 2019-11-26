ee163521677012e4e44aade15e46ad9fc2bc928c
Данил Панков <mistertimechip@mail.ru>
2019-11-27 01:21:54 +0300

    Finished index

--- cvs_trees/index.py
+++ cvs_trees/index.py
@@ -1,10 +1,34 @@
+import os
+
+from cvs_trees.head import Head
+from data_objects.commit import Commit
+
+
 class Index:
 
+    def __init__(self):
+        self.working_path = ''
+        self.indexed_files = set()
+        self.last_commit = Commit("NONE")
+
+    def set_working_path(self, path):
+        self.working_path = path
+
     def add_new_file(self, filename):
-        pass
+        if not self.is_file_in_working_directory(filename):
+            raise FileNotFoundError("No such file in file directory!")
+        self.indexed_files.add(filename)
 
-    def commit(self, commit_message):
-        pass
+    def is_file_in_working_directory(self, filename) -> bool:
+        return os.path.exists(self.working_path + filename)
 
-    def reset(self):
-        pass
+    def make_commit(self, commit_message) -> Commit:
+        commit = Commit(commit_message)
+        commit.freeze_files(self.indexed_files)
+        self.last_commit = commit
+        return commit
+
+    def reset(self, head: Head):
+        commit = head.get_current_branch().current_commit
+        self.indexed_files = commit.indexed_files
+        self.last_commit = commit
