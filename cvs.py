992ec5fd74a1cba7cf566c5e07b2efd6b42c3f30
Данил Панков <mistertimechip@mail.ru>
2019-11-27 01:21:21 +0300

    Fixed architecture Added woking path

--- cvs.py
+++ cvs.py
@@ -1,6 +1,6 @@
+import os
 from cvs_trees.index import Index
 from cvs_trees.working_directory import WorkingDirectory
-from data_objects.commit import Commit
 from data_objects.repostitory import Repository
 
 
@@ -10,17 +10,20 @@ class CVS:
         self.repository = Repository()
         self.index = Index()
         self.working_directory = WorkingDirectory()
+        self.working_path = ""
 
     def init(self):
+        self.__get_working_path()
         self.repository.init()
+        self.working_directory.set_working_path(self.working_path)
+        self.index.set_working_path(self.working_path)
         self.working_directory.find_not_indexed_files()
 
     def add(self, filename):
         self.index.add_new_file(filename)
 
     def commit(self, commit_message):
-        commit = Commit(commit_message)
-        commit.copy_from_index(self.index)
+        commit = self.index.make_commit(commit_message)
         self.repository.add_commit(commit)
         self.repository.point_to_last_commit()
 
@@ -39,12 +42,15 @@ class CVS:
 
     def mixed_reset(self):
         self.repository.reset_head()
-        self.index.reset()
+        self.index.reset(self.repository.head)
 
     def hard_reset(self):
         self.repository.reset_head()
-        self.index.reset()
+        self.index.reset(self.repository.head)
         self.working_directory.reset()
 
     def log(self) -> str:
         return self.repository.get_commit_history()
+
+    def __get_working_path(self):
+        self.working_path = os.path.dirname(os.path.abspath(__file__))
