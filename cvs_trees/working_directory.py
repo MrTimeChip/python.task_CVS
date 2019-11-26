624cd1a3bc7da8676d4c5d1149dc5f61ca108206
Данил Панков <mistertimechip@mail.ru>
2019-11-27 01:59:29 +0300

    Finished working directory

--- cvs_trees/working_directory.py
+++ cvs_trees/working_directory.py
@@ -1,10 +1,28 @@
+import os
+from os.path import join
+from shutil import copyfile
+
+from cvs_trees.index import Index
+
+
 class WorkingDirectory:
 
-    def get_current_status(self):
-        pass
+    def __init__(self):
+        self.working_path = ""
+        self.not_indexed_files = set()
 
-    def find_not_indexed_files(self):
-        pass
+    def set_working_path(self, path):
+        self.working_path = path
 
-    def reset(self):
-        pass
+    def find_not_indexed_files(self, indexed: set):
+        for file in os.listdir(self.working_path):
+            is_file = os.path.isfile(join(self.working_path, file))
+            if is_file and file not in indexed:
+                self.not_indexed_files.add(is_file)
+
+    def reset(self, index: Index):
+        for filename in index.indexed_files:
+            file_indexed = os.path.join(index.index_path, filename)
+            file_origin = os.path.join(self.working_path, filename)
+            copyfile(file_indexed, file_origin)
+
