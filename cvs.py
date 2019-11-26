55783d4f16fc5eb07c5e88ff5d66432b8ca30a49
Данил Панков <mistertimechip@mail.ru>
2019-11-27 00:40:03 +0300

    Finished cvs

--- cvs.py
+++ cvs.py
@@ -1,16 +1,52 @@
+from cvs_trees.head import Head
+from cvs_trees.index import Index
+from cvs_trees.working_directory import WorkingDirectory
+from data_objects.commit import Commit
+from data_objects.repostitory import Repository
+
+
 class CVS:
 
+    def __init__(self):
+        self.repository = Repository()
+        self.head = Head()
+        self.index = Index()
+        self.working_directory = WorkingDirectory()
+
     def init(self):
-        pass
+        self.repository.init(self.head)
+        self.working_directory.find_not_indexed_files()
 
     def add(self, filename):
-        pass
+        self.index.add_new_file(filename)
 
     def commit(self, commit_message):
-        pass
+        commit = Commit(commit_message)
+        commit.copy_from_index(self.index)
+        self.repository.add_commit(commit)
+        self.repository.point_to_last_commit()
 
     def reset(self, mode):
-        pass
+        if mode == '--soft':
+            self.soft_reset()
+            return
+        if mode == '-mixed' or mode == '':
+            self.mixed_reset()
+            return
+        if mode == '--hard':
+            self.hard_reset()
 
-    def log(self):
-        pass
+    def soft_reset(self):
+        self.head.reset()
+
+    def mixed_reset(self):
+        self.head.reset()
+        self.index.reset()
+
+    def hard_reset(self):
+        self.head.reset()
+        self.index.reset()
+        self.head.reset()
+
+    def log(self) -> str:
+        return self.repository.get_commit_history()
