241d47af48fa36e82e9e22a39aa8ba27a294df12
Данил Панков <mistertimechip@mail.ru>
2019-11-27 15:27:30 +0300

    Added directory info Added copying for commit

--- data_objects/repostitory.py
+++ data_objects/repostitory.py
@@ -1,6 +1,10 @@
 from cvs_trees.head import Head
 from data_objects.branch import Branch
 from data_objects.commit import Commit
+from shutil import copyfile
+import os
+
+from data_objects.directory_info import DirectoryInfo
 
 
 class Repository:
@@ -9,11 +13,19 @@ class Repository:
         self.current_branch = Branch("NONE")
         self.last_commit = Commit("NONE")
         self.head = Head()
+        self.directory = DirectoryInfo()
+
+    def set_directory_info(self, directory_info: DirectoryInfo):
+        self.directory = directory_info
 
     def add_commit(self, commit):
         if self.last_commit is None:
             self.last_commit = commit
-            return
+        for file, path in commit.get_copying_paths():
+            commit_path = self.directory.get_commits_path(
+                self.current_branch.name)
+            copy_path = os.path.join(commit_path, file)
+            copyfile(path, copy_path)
         commit.set_previous_commit(self.last_commit)
         self.last_commit = commit
 
