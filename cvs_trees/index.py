4057fd3340b25d137eb780a118cefb91c573d871
Данил Панков <mistertimechip@mail.ru>
2019-11-27 15:13:16 +0300

    Now uses DirectoryInfo

--- cvs_trees/index.py
+++ cvs_trees/index.py
@@ -3,42 +3,46 @@ from shutil import copyfile
 
 from cvs_trees.head import Head
 from data_objects.commit import Commit
+from data_objects.directory_info import DirectoryInfo
 
 
 class Index:
 
     def __init__(self):
-        self.working_path = ''
-        self.cvs_path = ""
-        self.index_path = ""
         self.indexed_files = set()
         self.last_commit = Commit("NONE")
+        self.directory = DirectoryInfo()
 
-    def set_working_path(self, path):
-        self.working_path = path
-
-    def set_cvs_path(self, path):
-        self.cvs_path = path
-        self.index_path = os.path.join(self.cvs_path, '/INDEX')
+    def set_directory_info(self, directory_info: DirectoryInfo):
+        """Sets current directory info"""
+        self.directory = directory_info
 
     def add_new_file(self, filename):
+        """Adds new file, copying it to /CVS/INDEX/..."""
         if not self.is_file_in_working_directory(filename):
             raise FileNotFoundError("No such file in file directory!")
         self.indexed_files.add(filename)
-        source_file = os.path.join(self.working_path, filename)
-        file_copy = os.path.join(self.index_path, filename)
+        source_file = os.path.join(self.directory.working_path, filename)
+        file_copy = os.path.join(self.directory.index_path, filename)
         copyfile(source_file, file_copy)
 
     def is_file_in_working_directory(self, filename) -> bool:
-        return os.path.exists(os.path.join(self.working_path, filename))
+        """Checks if file is in working directory"""
+        return os.path.exists(os.path.join(self.directory.working_path,
+                                           filename))
 
     def make_commit(self, commit_message) -> Commit:
+        """
+        Makes commit, freezing current files state
+        :returns Commit
+        """
         commit = Commit(commit_message)
-        commit.freeze_files(self.indexed_files, self.index_path)
+        commit.freeze_files(self.indexed_files, self.directory.index_path)
         self.last_commit = commit
         return commit
 
     def reset(self, head: Head):
+        """Resets index to last commit, that head is pointing to"""
         commit = head.get_current_branch().current_commit
         self.indexed_files = commit.indexed_files
         self.last_commit = commit
--- cvs_trees/working_directory.py
+++ cvs_trees/working_directory.py
@@ -3,26 +3,30 @@ from os.path import join
 from shutil import copyfile
 
 from cvs_trees.index import Index
+from data_objects.directory_info import DirectoryInfo
 
 
 class WorkingDirectory:
 
     def __init__(self):
-        self.working_path = ""
         self.not_indexed_files = set()
+        self.directory = DirectoryInfo()
 
-    def set_working_path(self, path):
-        self.working_path = path
+    def set_directory_info(self, directory_info: DirectoryInfo):
+        """Sets current directory info"""
+        self.directory = directory_info
 
     def find_not_indexed_files(self, indexed: set):
-        for file in os.listdir(self.working_path):
-            is_file = os.path.isfile(join(self.working_path, file))
+        """Finds not indexed files"""
+        for file in os.listdir(self.directory.working_path):
+            is_file = os.path.isfile(join(self.directory.working_path, file))
             if is_file and file not in indexed:
                 self.not_indexed_files.add(is_file)
 
     def reset(self, index: Index):
+        """Rewrites file in current working directory"""
         for filename in index.indexed_files:
-            file_indexed = os.path.join(index.index_path, filename)
-            file_origin = os.path.join(self.working_path, filename)
+            file_indexed = os.path.join(self.directory.index_path, filename)
+            file_origin = os.path.join(self.directory.working_path, filename)
             copyfile(file_indexed, file_origin)
 
