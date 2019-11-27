16385b247d1e42117bfdea90dc5f567f7e02e0d8
Данил Панков <mistertimechip@mail.ru>
2019-11-28 02:13:24 +0300

    Wrote tests and finished realisation

--- data_objects/directory_info.py
+++ data_objects/directory_info.py
@@ -1,3 +1,4 @@
+import copy
 import os
 
 
@@ -7,32 +8,41 @@ class DirectoryInfo:
         self.working_path = ""
         self.cvs_path = ""
         self.index_path = ""
-        self.branches_commits_path = {}
-        self.branches_path = {}
+        self.__branches_commits_paths = {}
+        self.__branches_paths = {}
+
+    @property
+    def branches_paths(self):
+        return copy.copy(self.__branches_paths)
+
+    @property
+    def branches_commits_paths(self):
+        return copy.copy(self.__branches_commits_paths)
 
     def init(self):
         """Initializes paths"""
-        self.working_path = os.path.dirname(os.path.abspath(__file__))
-        self.cvs_path = os.path.join(self.working_path, "/CVS")
-        self.index_path = os.path.join(self.cvs_path, "/INDEX")
+        self.working_path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__),
+                         '..'))
+        self.cvs_path = os.path.join(self.working_path, "CVS")
+        self.index_path = os.path.join(self.cvs_path, "INDEX")
 
     def add_branch_path(self, branch_name):
         """Adds branch to paths"""
-        path_to_branch = os.path.join(self.cvs_path, "/" + branch_name)
-        self.branches_path = path_to_branch
+        path_to_branch = os.path.join(self.cvs_path, branch_name)
+        self.__branches_paths[branch_name] = path_to_branch
         path_to_commits = os.path.join(self.cvs_path,
-                                       "/" + branch_name + "/COMMITS")
-        self.branches_commits_path[branch_name] = path_to_commits
+                                       branch_name + "\\COMMITS")
+        self.__branches_commits_paths[branch_name] = path_to_commits
 
-    def get_branch_path(self, branch_name):
+    def get_branch_path(self, branch_name) -> str:
         """Returns branch path"""
-        if branch_name not in self.branches_path.keys():
+        if branch_name not in self.__branches_paths.keys():
             raise ValueError("No branch found!")
-        return self.branches_path[branch_name]
+        return self.__branches_paths[branch_name]
 
-    def get_commits_path(self, branch_name):
+    def get_commits_path(self, branch_name) -> str:
         """Return commits path for branch"""
-        if branch_name not in self.branches_path.keys():
+        if branch_name not in self.__branches_paths.keys():
             raise ValueError("No branch found!")
-        return self.branches_commits_path[branch_name]
-
+        return self.__branches_commits_paths[branch_name]
---
+++ tests/test_directory_info.py
@@ -0,0 +1,51 @@
+import unittest
+
+from data_objects.directory_info import DirectoryInfo
+
+
+class DirectoryInfoTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.info = DirectoryInfo()
+
+    def test_init_should_initialize_working_path(self):
+        self.info.init()
+        result = self.info.working_path
+        self.assertIsNotNone(result)
+
+    def test_init_should_initialize_cvs_path(self):
+        self.info.init()
+        result = self.info.cvs_path
+        self.assertTrue(result.endswith('CVS'))
+
+    def test_init_should_initialize_index_path(self):
+        self.info.init()
+        result = self.info.index_path
+        self.assertTrue(result.endswith('CVS\\INDEX'))
+
+    def test_add_branch_path_should_add_path_to_branch(self):
+        self.info.init()
+        self.info.add_branch_path("master")
+        count = len(self.info.branches_paths)
+        self.assertGreater(count, 0)
+
+    def test_add_branch_path_should_add_path_to_commits(self):
+        self.info.init()
+        self.info.add_branch_path("master")
+        count = len(self.info.branches_commits_paths)
+        self.assertGreater(count, 0)
+
+    def test_get_branch_path_should_get_path_to_branch(self):
+        self.info.init()
+        self.info.add_branch_path("master")
+        path = self.info.get_branch_path("master")
+        self.assertTrue(path.endswith("CVS\\master"))
+
+    def test_get_commits_path_should_get_commits_path(self):
+        self.info.init()
+        self.info.add_branch_path("master")
+        path = self.info.get_commits_path("master")
+        self.assertTrue(path.endswith("CVS\\master\\COMMITS"))
+
+
+if __name__ == '__main__':
+    unittest.main()
