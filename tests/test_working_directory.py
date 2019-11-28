bc1dd813268d94064bf7474a0e81e91526eccca8
Данил Панков <mistertimechip@mail.ru>
2019-11-28 13:48:27 +0300

    Finished tests

--- tests/test_working_directory.py
+++ tests/test_working_directory.py
@@ -1,5 +1,8 @@
+import os
+import shutil
 import unittest
 
+from cvs_trees.index import Index
 from cvs_trees.working_directory import WorkingDirectory
 from data_objects.directory_info import DirectoryInfo
 
@@ -7,9 +10,20 @@ from data_objects.directory_info import DirectoryInfo
 class WorkingDirectoryTest(unittest.TestCase):
     def setUp(self) -> None:
         self.wd = WorkingDirectory()
-        di = DirectoryInfo()
-        di.init()
-        self.wd.set_directory_info(di)
+        self.di = DirectoryInfo()
+        path = os.path.join(os.getenv('APPDATA'), 'TESTING')
+        self.di.set_custom_path(path)
+        self.di.init()
+        if not os.path.exists(path):
+            os.makedirs(path)
+        self.file_path = os.path.join(self.di.working_path, 'TESTING.txt')
+        with open(self.file_path, "w+") as file:
+            file.write('SOME STRING')
+        self.wd.set_directory_info(self.di)
+
+    def tearDown(self) -> None:
+        if os.path.exists(self.di.cvs_path):
+            shutil.rmtree(self.di.cvs_path)
 
     def test_find_not_indexed_files_should_find_when_empty_indexed(self):
         self.wd.find_not_indexed_files(set())
@@ -18,9 +32,15 @@ class WorkingDirectoryTest(unittest.TestCase):
     def test_find_not_indexed_files_should_return_files_in_working_path(self):
         self.wd.find_not_indexed_files(set())
         result = self.wd.not_indexed_files
-        self.assertTrue("cvs.py" in result)
+        self.assertTrue("TESTING.txt" in result)
 
-    #TODO: write reset test
+    def test_reset_should_rewrite_files_from_index(self):
+        index = Index()
+        index.set_directory_info(self.di)
+        index.add_new_file('TESTING.txt')
+        os.remove(self.file_path)
+        self.wd.reset(index)
+        self.assertTrue(os.path.exists(self.file_path))
 
 
 if __name__ == '__main__':
