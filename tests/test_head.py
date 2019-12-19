9f9ae9288827f7f131091da56c94e8d35485356c
Данил Панков <mistertimechip@mail.ru>
2019-12-19 19:33:11 +0300

    Fixed test.

--- tests/test_head.py
+++ tests/test_head.py
@@ -24,11 +24,12 @@ class TestHead(unittest.TestCase):
         prev_commit.branch_name = 'master'
         prev_commit.init_config()
         commit.set_previous_commit_number(prev_commit.commit_number)
+        previous_commit_number = commit.get_previous_commit_number()
         head.current_branch.set_current_commit(commit)
         head.reset()
         current_commit = head.current_branch.get_current_commit()
         self.assertEqual(current_commit.commit_number,
-                         commit.get_previous_commit_number())
+                         previous_commit_number)
 
 
 if __name__ == '__main__':
